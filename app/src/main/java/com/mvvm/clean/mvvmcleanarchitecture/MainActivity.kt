7cd093e86a2c95052122b6977dac8582900c9b21
gaurav <gauravpete@gmail.com>
2025-06-12 22:55:35 +0530

    Set background for screen

--- app/src/main/java/com/mvvm/clean/mvvmcleanarchitecture/MainActivity.kt
+++ app/src/main/java/com/mvvm/clean/mvvmcleanarchitecture/MainActivity.kt
@@ -4,6 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import com.mvvm.clean.mvvmcleanarchitecture.ui.theme.MVVMCleanArchitectureTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,9 +18,27 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MVVMCleanArchitectureTheme {
-
+                App()
             }
         }
+    }
+}
+
+@Composable
+fun App() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    listOf(
+                        androidx.compose.ui.graphics.Color(0xFFBB86FC),
+                        androidx.compose.ui.graphics.Color(0xFF3700B3)
+                    )
+                )
+            )
+    ) {
+
     }
 }
 
