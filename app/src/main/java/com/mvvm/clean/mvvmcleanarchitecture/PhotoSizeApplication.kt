aac5813539981011381752889281bb379efed2da
gaurav <gauravpete@gmail.com>
2025-06-15 18:31:09 +0530

    create the folder structure

---
+++ app/src/main/java/com/mvvm/clean/mvvmcleanarchitecture/PhotoSizeApplication.kt
@@ -0,0 +1,8 @@
+package com.mvvm.clean.mvvmcleanarchitecture
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PhotoSizeApplication : Application() {
+}
