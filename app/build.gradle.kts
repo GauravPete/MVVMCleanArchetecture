5bdf3c768e11e128515ce04498cb4af0ce704015
gaurav <gauravpete@gmail.com>
2025-06-15 15:52:33 +0530

    remove the warning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,6 +92,6 @@ dependencies {
     // optional - Test helpers for Lifecycle runtime
     testImplementation(libs.androidx.lifecycle.runtime.testing)
 
-    implementation(platform("com.google.firebase:firebase-bom:33.15.0"))
-    implementation("com.google.firebase:firebase-analytics")
+    implementation(libs.firebase.bom)
+    implementation(libs.firebase.analytics)
 }
